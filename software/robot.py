c6fcc12aeeab91cdfcfdfef16ee9d58f1fcc4f9c
Timo654 <41972888+Timo654@users.noreply.github.com>
2022-09-12 20:39:53 +0300

    switched to while loop, added a delay

--- software/robot.py
+++ software/robot.py
@@ -1,7 +1,7 @@
 from motion import IRobotMotion
 import serial
 import struct
-
+import time
 
 class AwesomeRobot(IRobotMotion):
     def __init__(self, port):
@@ -21,22 +21,26 @@ class AwesomeRobot(IRobotMotion):
     # delimiter uint16_t
     # the base of this code was written by rasmus
     def send(self, speed1, speed2, speed3, throwerSpeed, disableFailsafe=0, delimiter=0xAAAA):
+        print("Tere Timo ja Jens!!!!!!!!!!")
         """         if (speed1 < 0):
             speed1 += 2 ** 16
         if (speed2 < 0):
             speed2 += 2 ** 16
         if (speed3 < 0):
             speed3 += 2 ** 16 """
-        print(struct.pack('<hhhHBH', speed1, speed2, speed3, throwerSpeed, disableFailsafe, delimiter))
-        self.serial.write(struct.pack('<hhhHBH', speed1, speed2, speed3, throwerSpeed, disableFailsafe, delimiter))
+        sent_data = struct.pack('<hhhHBH', speed1, speed2, speed3, throwerSpeed, disableFailsafe, delimiter)
+        self.serial.write(sent_data)
+        print("sent:", sent_data)
         received_data = self.serial.read(8)
         actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack('<hhhH', received_data)
-        print(actual_speed1, actual_speed2, actual_speed3)
+        print("received", actual_speed1, actual_speed2, actual_speed3)
         # i hate python - Artur
 
 
 
 if __name__ == "__main__":
     robot = AwesomeRobot('/dev/ttyACM0')
-    for i in range(5):
-        robot.send(-16, -16, -16, 0)
+    while True:
+        robot.send(10, 10, 10,0)
+        time.sleep(0.1)
+        
