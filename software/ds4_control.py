bda738ecb2a0d1c2d0d407e376c542653c472643
Timo654 <41972888+Timo654@users.noreply.github.com>
2022-10-15 17:15:13 +0300

    multi-input support

--- software/ds4_control.py
+++ software/ds4_control.py
@@ -1,7 +1,7 @@
 import motion
 from pyPS4Controller.controller import Controller
 
-# TODO - thrower and figure out what values to actually feed the robot
+# TODO - thrower
 
 
 class RobotDS4(Controller):
@@ -9,38 +9,73 @@ class RobotDS4(Controller):
         Controller.__init__(self, **kwargs)
         self.robot = motion.OmniRobot()
         self.robot.open()
-        self.speed = 0.5
-        print("Controller connected.")
+        self.max_speed = 1
+        self.x_speed = 0
+        self.y_speed = 0
+        self.rot_speed = 0
+        self.thrower_speed = 0
+        print("Controller added.")
+
+    def send_movement(self):
+        self.robot.move(self.x_speed, self.y_speed, self.rot_speed,
+                        self.thrower_speed, disableFailsafe=True)
 
     # steering
     def on_left_arrow_press(self):
         print("Left - Move")
-        self.robot.move(self.speed, 0, 0, 0, disableFailsafe=1)
+        self.x_speed = -self.max_speed
+        self.send_movement()
 
     def on_right_arrow_press(self):
         print("Right - move")
-        self.robot.move(-self.speed, 0, 0, 0, disableFailsafe=1)
+        self.x_speed = self.max_speed
+        self.send_movement()
 
     def on_left_right_arrow_release(self):
-        self.robot.stop()
-        print("Steering - stop")
+        print("L/R - stop")
+        self.x_speed = 0
+        self.send_movement()
 
     # triggers had duplicated signals, so I opted for bumpers for now
     def on_R1_press(self):
-        self.robot.move(0, self.speed, 0, 0, disableFailsafe=1)
         print("R1 - forward")
+        self.y_speed = self.max_speed
+        self.send_movement()
 
     def on_R1_release(self):
         print("R1 - stop")
-        self.robot.stop()
+        self.y_speed = 0
+        self.send_movement()
 
     def on_L1_press(self):
         print("L1 - reverse")
-        self.robot.move(0, -self.speed, 0, 0, disableFailsafe=1)
+        self.y_speed = -self.max_speed
+        self.send_movement()
 
     def on_L1_release(self):
         print("L1 - stop")
-        self.robot.stop()
+        self.y_speed = 0
+        self.send_movement()
+
+    def on_circle_press(self):
+        print("Circle (L) - rot. left")
+        self.rot_speed = -(3*self.max_speed)
+        self.send_movement()
+
+    def on_circle_release(self):
+        print("Circle (L) - stop")
+        self.rot_speed = 0
+        self.send_movement()
+
+    def on_square_press(self):
+        print("Square (R) - rot. right")
+        self.rot_speed = 3*self.max_speed
+        self.send_movement()
+
+    def on_square_release(self):
+        print("Square (R) - stop")
+        self.rot_speed = 0
+        self.send_movement()
 
     # quit
     def on_playstation_button_press(self):
