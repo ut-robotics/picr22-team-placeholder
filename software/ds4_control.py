4bbe990ba4171e33e56569c19d4d08782c45c7c4
Timo654 <41972888+Timo654@users.noreply.github.com>
2022-10-18 15:37:04 +0300

    switch to analog inputs for DS4, add thrower
    also some cleanup in general

--- software/ds4_control.py
+++ software/ds4_control.py
@@ -1,7 +1,8 @@
 import motion
+from helper import map_range
 from pyPS4Controller.controller import Controller
 
-# TODO - thrower
+# TODO - implement mode changing
 
 
 class RobotDS4(Controller):
@@ -13,69 +14,108 @@ class RobotDS4(Controller):
         self.x_speed = 0
         self.y_speed = 0
         self.rot_speed = 0
-        self.thrower_speed = 0
+        self.thrower_speed = 0  # speed currently
+        self.thrower_speed_param = 50  # speed to use when we activate thrower
         print("Controller added.")
 
     def send_movement(self):
         self.robot.move(self.x_speed, self.y_speed, self.rot_speed,
                         self.thrower_speed, disableFailsafe=True)
 
-    # steering
-    def on_left_arrow_press(self):
-        print("Left - Move")
-        self.x_speed = -self.max_speed
+    def axis_stop(self, axis):
+        if axis == "x":
+            self.x_speed = 0
+        elif axis == "y":
+            self.y_speed = 0
+        elif axis == "rot":
+            self.rot_speed = 0
+        elif axis == "thrower":
+            self.thrower_speed = 0
+        else:
+            print("Unknown axis", axis)
+
+    # thrower
+    def on_up_arrow_press(self):
+        self.thrower_speed_param += 1
+        print("Increased thrower speed by 1, new speed:",
+              self.thrower_speed_param)
+
+    def on_down_arrow_press(self):
+        if self.thrower_speed_param == 49:
+            print("Thrower - Already at minimum speed!")
+        else:
+            self.thrower_speed_param -= 1
+            print("Decreased thrower speed by 1, new speed:",
+                  self.thrower_speed_param)
+
+    def on_triangle_press(self):
+        print("Triangle - Thrower")
+        self.thrower_speed = self.thrower_speed_param
         self.send_movement()
 
-    def on_right_arrow_press(self):
-        print("Right - move")
+    def on_triangle_release(self):
+        print("Triangle - Thrower stop")
+        self.axis_stop("thrower")
+        self.send_movement()
+
+    # X-axis
+    def on_R1_press(self):
+        print("R1 - right")
         self.x_speed = self.max_speed
         self.send_movement()
 
-    def on_left_right_arrow_release(self):
-        print("L/R - stop")
-        self.x_speed = 0
-        self.send_movement()
-
-    # triggers had duplicated signals, so I opted for bumpers for now
-    def on_R1_press(self):
-        print("R1 - forward")
-        self.y_speed = self.max_speed
-        self.send_movement()
-
     def on_R1_release(self):
-        print("R1 - stop")
-        self.y_speed = 0
-        self.send_movement()
+        self.axis_stop("x")
 
     def on_L1_press(self):
-        print("L1 - reverse")
-        self.y_speed = -self.max_speed
+        print("L1 - left")
+        self.x_speed = -self.max_speed
         self.send_movement()
 
     def on_L1_release(self):
-        print("L1 - stop")
-        self.y_speed = 0
+        self.axis_stop("x")
+
+    # Y-axis
+    def on_R2_press(self, value):
+        print("R2 - forwards")
+        self.y_speed = map_range(value, -32767, 32767,
+                                 0, self.max_speed * 1000) / 1000
         self.send_movement()
 
-    def on_circle_press(self):
-        print("Circle (L) - rot. left")
-        self.rot_speed = -(3*self.max_speed)
+    def on_R2_release(self):
+        self.axis_stop("y")
+
+    def on_L2_press(self, value):
+        print("L2 - backwards")
+        self.y_speed = map_range(value, -32767, 32767,
+                                 0, -self.max_speed * 1000) / 1000
         self.send_movement()
 
-    def on_circle_release(self):
-        print("Circle (L) - stop")
-        self.rot_speed = 0
+    def on_L2_release(self):
+        self.axis_stop("y")
+
+    # TODO - figure out deadzones or go back to old system
+    # Rotation
+    def on_L3_left(self, value):
+        print("L3 - rot. left")
+        self.rot_speed = map_range(
+            value, 0, -32767, 0, self.max_speed * 3000) / 1000
         self.send_movement()
 
-    def on_square_press(self):
-        print("Square (R) - rot. right")
-        self.rot_speed = 3*self.max_speed
+    def on_L3_right(self, value):
+        print("L3 - rot. right")
+        self.rot_speed = map_range(
+            value, 0, 32767, 0, -self.max_speed * 3000) / 1000
         self.send_movement()
 
-    def on_square_release(self):
-        print("Square (R) - stop")
-        self.rot_speed = 0
-        self.send_movement()
+    def on_L3_x_at_rest(self):
+        self.axis_stop("rot")
+
+    # MISC
+
+    # switch modes
+    def on_share_press(self):
+        print("TODO - implement switching between autonomous and normal mode.")
 
     # quit
     def on_playstation_button_press(self):
