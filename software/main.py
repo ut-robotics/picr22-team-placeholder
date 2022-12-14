7558746847274ac1787aa0775e6eb2786d4b7bf5
Timo654 <41972888+Timo654@users.noreply.github.com>
2022-12-14 17:12:36 +0200

    potentially improve fps

--- software/main.py
+++ software/main.py
@@ -169,7 +169,10 @@ class Robot:
     def get_image_data(self):
         """Main non-state part of the loop"""
         # has argument aligned_depth that enables depth frame to color frame alignment. Costs performance
-        self.processed_data = self.processor.process_frame(aligned_depth=True)
+        if self.current_state in [State.Searching, State.Stopped, State.EscapeFromBasket]: # don't use it when we don't need it, TODO - verify it doesnt break anything
+            self.processed_data = self.processor.process_frame(aligned_depth=False)
+        else:
+            self.processed_data = self.processor.process_frame(aligned_depth=True)
 
         # -- BOILERPLATE CAMERA CODE, DO NOT TOUCH UNLESS REALLY NECESSARY --
         # FPS counter
